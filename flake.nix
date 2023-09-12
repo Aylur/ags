89c6a855d542d27685159abc8ea82ad773750f95
Jappie3 <jasper22034@gmail.com>
2023-09-12 19:00:20 +0200

    feat: add hm module (#90)

--- flake.nix
+++ flake.nix
@@ -5,19 +5,21 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { nixpkgs, self }:
-  let
+  outputs = {
+    nixpkgs,
+    self,
+  }: let
     genSystems = nixpkgs.lib.genAttrs [
       "aarch64-linux"
       "x86_64-linux"
     ];
-    pkgs = genSystems (system: import nixpkgs { inherit system; });
-  in 
-  {
+    pkgs = genSystems (system: import nixpkgs {inherit system;});
+  in {
     packages = genSystems (system: {
       default = pkgs.${system}.callPackage ./nix/ags.nix {
-        gjs = pkgs.${system}.callPackage ./nix/gjs.nix { };
+        gjs = pkgs.${system}.callPackage ./nix/gjs.nix {};
       };
     });
+    homeManagerModules.default = import ./nix/hm-module.nix self;
   };
 }
