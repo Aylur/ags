0bd14ef523f2cfce6d48204f7db9bee86cdf18bd
matt1432 <98562002+matt1432@users.noreply.github.com>
2023-09-18 15:19:17 -0400

    add: widget css property (#100)

--- src/widgets/overrides.ts
+++ src/widgets/overrides.ts
@@ -37,19 +37,14 @@ Object.defineProperty(Gtk.Widget.prototype, 'className', {
 });
 
 const widgetProviders: Map<Gtk.Widget, Gtk.CssProvider> = new Map();
-function setStyle(widget: Gtk.Widget, css: string) {
-    if (typeof css !== 'string') {
-        console.error('style has to be a string');
-        return false;
-    }
-
+function setCss(widget: Gtk.Widget, css: string) {
     const previous = widgetProviders.get(widget);
     if (previous)
         widget.get_style_context().remove_provider(previous);
 
     const provider = new Gtk.CssProvider();
     widgetProviders.set(widget, provider);
-    provider.load_from_data(`* { ${css} }`);
+    provider.load_from_data(css);
     widget.get_style_context()
         .add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_USER);
 }
@@ -59,16 +54,39 @@ Object.defineProperty(Gtk.Widget.prototype, 'style', {
         return this._style || '';
     },
     set: function(css: string) {
-        if (!setStyle(this, css))
+        if (typeof css !== 'string') {
+            console.error('style has to be a string');
             return;
+        }
 
+        setCss(this, `* { ${css} }`);
         this._style = css;
     },
 });
 
+Object.defineProperty(Gtk.Widget.prototype, 'css', {
+    get: function() {
+        return this._css || '';
+    },
+    set: function(css: string) {
+        if (typeof css !== 'string') {
+            console.error('css has to be a string');
+            return;
+        }
+
+        setCss(this, css);
+        this._css = css;
+    },
+});
+
+// @ts-expect-error
+Gtk.Widget.prototype.setCss = function(css: string) {
+    setCss(this, css);
+};
+
 // @ts-expect-error
 Gtk.Widget.prototype.setStyle = function(css: string) {
-    setStyle(this, css);
+    setCss(this, `* { ${css} }`);
 };
 
 // @ts-expect-error
