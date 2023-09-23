195e672a0c0971703a320221c94fd2c62e0af822
kotontrion <kotontrion@tutanota.de>
2023-09-23 11:17:33 +0200

    added window and workspace handlers

--- src/service/sway.ts
+++ src/service/sway.ts
@@ -20,7 +20,7 @@ const enum PAYLOAD_TYPE {
     MESSAGE_GET_BINDING_STATE = 12,
     MESSAGE_GET_INPUTS = 100,
     MESSAGE_GET_SEATS = 101,
-    EVENT_WORKSPACE = 0x8000000,
+    EVENT_WORKSPACE = 0x80000000,
     EVENT_MODE = 0x80000002,
     EVENT_WINDOW =  0x80000003,
     EVENT_BARCONFIG_UPDATE = 0x80000004,
@@ -29,6 +29,81 @@ const enum PAYLOAD_TYPE {
     EVENT_TICK =  0x80000007,
     EVENT_BAR_STATE_UPDATE = 0x80000014,
     EVENT_INPUT = 0x80000015,
+}
+
+interface Client_Event {
+    change: string,
+    container: Node,
+}
+
+interface Workspace_Event {
+    change: string,
+    current: Node,
+    old: Node,
+}
+
+interface Geometry {
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+}
+
+//NOTE: not all properties are listed here
+interface Node {
+    id: number,
+    name: string,
+    type: string,
+    border: string
+    current_border_width: number
+    layout:  string
+    orientation: string
+    percent: number
+    rect: Geometry
+    window_rect: Geometry
+    deco_rect: Geometry
+    geometry: Geometry
+    urgent: boolean
+    sticky: boolean
+    marks: [string]
+    focused: boolean
+    focus: [number]
+    nodes: [Node]
+    floating_nodes: [Node]
+    representation: string
+    fullscreen_mode: number
+    app_id: string
+    pid: number
+    visible: boolean
+    shell: string
+    output: string,
+    inhibit_idle: boolean
+    idle_inhibitors: {
+        application: string,
+        user: string,
+    }
+    window: number
+    window_properties: {
+        title: string,
+        class: string,
+        instance: string,
+        window_role: string,
+        window_type: string,
+        transient_for: string,
+    }
+}
+
+interface Active {
+    client: {
+        id: number
+        title: string
+        class: string
+    }
+    monitor: string
+    workspace: {
+        id: number
+        name: string
+    }
 }
 
 class SwayService extends Service {
@@ -40,10 +115,38 @@ class SwayService extends Service {
     private _encoder = new TextEncoder();
     private _output_stream: Gio.OutputStream;
 
+    private _active: Active;
+    private _monitors: Map<number, object>;
+    private _workspaces: Map<number, object>;
+    private _clients: Map<number, Node>;
+
+    get active() { return this._active; }
+    get monitors() { return this._monitors; }
+    get workspaces() { return this._workspaces; }
+    get clients() { return this._clients; }
+
     constructor() {
         if (!SIS)
             console.error('Sway is not running');
         super();
+
+        this._active = {
+            client: {
+                id: 0,
+                title: '',
+                class: '',
+            },
+            monitor: '',
+            workspace: {
+                id: 0,
+                name: '',
+            },
+        };
+
+        this._monitors = new Map();
+        this._workspaces = new Map();
+        this._clients = new Map();
+
         const socket = new Gio.SocketClient().connect(new Gio.UnixSocketAddress({
             path: `${SIS}`,
         }), null);
@@ -51,6 +154,7 @@ class SwayService extends Service {
         this._watchSocket(socket.get_input_stream());
 
         this._output_stream = socket.get_output_stream();
+        //TODO init everything by mybe using get_tree
         this.send(PAYLOAD_TYPE.MESSAGE_SUBSCRIBE, JSON.stringify(['window', 'workspace']));
     }
 
@@ -64,7 +168,6 @@ class SwayService extends Service {
             ...(new Uint8Array(pl.buffer)),
             ...(new Uint8Array(type.buffer)),
             ...pb]);
-        print(data);
         this._output_stream.write_bytes(data, null);
     }
 
@@ -89,14 +192,16 @@ class SwayService extends Service {
         });
     }
 
-    private async _onEvent(event_type: PAYLOAD_TYPE, event: string) {
+    private async _onEvent(event_type: PAYLOAD_TYPE, event: object) {
         if (!event)
             return;
         try {
             switch (event_type) {
                 case PAYLOAD_TYPE.EVENT_WORKSPACE:
+                    this._handle_workspace_event(event as Workspace_Event);
                     break;
                 case PAYLOAD_TYPE.EVENT_WINDOW:
+                    this._handle_window_event(event as Client_Event);
                     break;
                 default:
                     break;
@@ -105,6 +210,74 @@ class SwayService extends Service {
             logError(error as Error);
         }
         this.emit('changed');
+    }
+
+    private _handle_workspace_event(workspace_event: Workspace_Event) {
+        const workspace = workspace_event.current;
+        switch (workspace_event.change) {
+            case 'init':
+                this._workspaces.set(workspace.id, workspace);
+                break;
+            case 'empty':
+                this._workspaces.delete(workspace.id);
+                break;
+            case 'focus':
+                this._active.workspace.id = workspace.id;
+                this._active.workspace.name = workspace.name;
+                this._active.monitor = workspace.output;
+                this._workspaces.set(workspace.id, workspace);
+                this._workspaces.set(workspace_event.old.id, workspace_event.old);
+                break;
+            case 'rename':
+                if (this._active.workspace.id === workspace.id)
+                    this._active.workspace.name = workspace.name;
+                this._workspaces.set(workspace.id, workspace);
+                break;
+            case 'reload':
+                break;
+            case 'move':
+            case 'urgent':
+            default:
+                this._workspaces.set(workspace.id, workspace);
+        }
+    }
+
+    private _handle_window_event(client_event: Client_Event) {
+        const client = client_event.container;
+        const id = client.id;
+        switch (client_event.change) {
+            case 'new':
+                this._clients.set(id, client);
+                break;
+            case 'close':
+                this._clients.delete(id);
+                break;
+            case 'focus':
+                if (this._active.client.id === id)
+                    return;
+                // eslint-disable-next-line no-case-declarations
+                const current_active = this._clients.get(this._active.client.id);
+                if (current_active)
+                    current_active.focused = false;
+                this._active.client.id = id;
+                this._active.client.title = client.name;
+                this._active.client.class = client.shell === 'xwayland'
+                    ? client.window_properties?.class || ''
+                    : client.app_id;
+                break;
+            case 'title':
+                if (client.focused)
+                    this._active.client.title = client.name;
+                this._clients.set(id, client);
+                break;
+            case 'fullscreen_mode':
+            case 'move':
+            case 'floating':
+            case 'urgent':
+            case 'mark':
+            default:
+                this._clients.set(id, client);
+        }
     }
 }
 
@@ -115,4 +288,9 @@ export default class Sway {
         Service.ensureInstance(Sway, SwayService);
         return Sway._instance;
     }
+
+    static get monitors() { return Array.from(Sway.instance.monitors.values()); }
+    static get workspaces() { return Array.from(Sway.instance.workspaces.values()); }
+    static get clients() { return Array.from(Sway.instance.clients.values()); }
+    static get active() { return Sway.instance.active; }
 }
