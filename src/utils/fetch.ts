b76758695dce2f88627176979b16cc760bca2d03
kotontrion <kotontrion@tutanota.de>
2024-01-01 09:05:48 +0100

    add url params to fetch

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -11,16 +11,29 @@ export type FetchOptions = {
     method?: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH';
     body?: string;
     headers?: Record<string, string>;
-    params?: Record<string, string>;
+    params?: Record<string, any>;
 };
 
 export async function fetch(url: string, options: FetchOptions = {}) {
     const session = new Soup.Session();
 
     if (options.params) {
-        url += '?' + Object.entries(options.params).map(([k, v]) =>
-            `${encodeURIComponent(k)}=${encodeURIComponent(v)}`).join('&');
+        url += '?' + Object.entries(options.params)
+            .map(([key, value]) => {
+                if (Array.isArray(value)) {
+                    return value.map(val =>
+                        `${encodeURIComponent(key)}=${encodeURIComponent(val)}`).join('&');
+                } else if (typeof value === 'object') {
+                    return `${encodeURIComponent(key)}=${encodeURIComponent(
+                        JSON.stringify(value))}`;
+                } else {
+                    return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+                }
+            })
+            .join('&');
     }
+
+    print(url)
 
     const message = new Soup.Message({
         method: options.method || 'GET',
