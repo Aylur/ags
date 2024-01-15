085aa2f80656324910b06f65baaae07b70e21ed5
Aylur <demstof@gmail.com>
2024-01-15 14:12:47 +0100

    exit on no config

--- src/app.ts
+++ src/app.ts
@@ -214,7 +214,12 @@ export class App extends Gtk.Application {
 
             this.emit('config-parsed');
         } catch (err) {
-            logError(err);
+            if ((err as { name: string }).name === 'ImportError') {
+                print(`config file not found: "${this._configPath}"`);
+                this.quit();
+            } else {
+                logError(err);
+            }
         }
     }
 
