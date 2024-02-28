f905abfd2210701929ffaa3db13faa783b782765
Aylur <demstof@gmail.com>
2024-02-28 02:37:23 +0100

    fix App.closeWindowDelay

--- src/app.ts
+++ src/app.ts
@@ -67,8 +67,8 @@ export class App extends Gtk.Application {
     private _configPath!: string;
     private _configDir!: string;
 
-    private _closeWindowDelay!: Config['closeWindowDelay'];
-    get closeWindowDelay() { return this._closeWindowDelay || {}; }
+    private _closeWindowDelay: Config['closeWindowDelay'] = {};
+    get closeWindowDelay() { return this._closeWindowDelay!; }
     set closeWindowDelay(v) { this._closeWindowDelay = v; }
 
     get windows() { return [...this._windows.values()]; }
