ae510ab25a8ab63a7317ca65cae918a21b2a7813
Aylur <demstof@gmail.com>
2024-04-29 12:59:09 +0200

    fix lint and reduce code redundancy

--- src/service/hyprland.ts
+++ src/service/hyprland.ts
@@ -3,11 +3,9 @@ import Gio from 'gi://Gio';
 import Service from '../service.js';
 
 Gio._promisify(Gio.DataInputStream.prototype, 'read_upto_async');
-const HIS = GLib.getenv('HYPRLAND_INSTANCE_SIGNATURE');
-const XDG_RUNTIME_DIR = GLib.getenv('XDG_RUNTIME_DIR');
 
-const socket = (path: string) => new Gio.SocketClient()
-    .connect(new Gio.UnixSocketAddress({ path }), null);
+const HIS = GLib.getenv('HYPRLAND_INSTANCE_SIGNATURE');
+const XDG_RUNTIME_DIR = GLib.getenv('XDG_RUNTIME_DIR') || '/';
 
 export class ActiveClient extends Service {
     static {
@@ -137,20 +135,24 @@ export class Hyprland extends Service {
         for (const c of JSON.parse(this.message('j/clients')) as Client[])
             this._clients.set(c.address, c);
 
-
-        // this._syncWorkspaces();
-        // this._syncClients();
-
-        const socketPath = GLib.file_test(`${XDG_RUNTIME_DIR}/hypr/${HIS}/.socket2.sock`, GLib.FileTest.EXISTS) ?
-                                `${XDG_RUNTIME_DIR}/hypr/${HIS}/.socket2.sock` : `/tmp/hypr/${HIS}/.socket2.sock`;
-
         this._watchSocket(new Gio.DataInputStream({
             close_base_stream: true,
-            base_stream: socket(socketPath)
+            base_stream: this._connection('socket2')
                 .get_input_stream(),
         }));
 
         this._active.connect('changed', () => this.changed('active'));
+    }
+
+    private _connection(socket: 'socket' | 'socket2') {
+        const sock = (pre: string) => `${pre}/hypr/${HIS}/.${socket}.sock`;
+
+        const path = GLib.file_test(sock(XDG_RUNTIME_DIR), GLib.FileTest.EXISTS)
+            ? sock(XDG_RUNTIME_DIR)
+            : sock('/tmp');
+
+        return new Gio.SocketClient()
+            .connect(new Gio.UnixSocketAddress({ path }), null);
     }
 
     private _watchSocket(stream: Gio.DataInputStream) {
@@ -172,9 +174,7 @@ export class Hyprland extends Service {
     };
 
     private _socketStream(cmd: string) {
-        const socketPath = GLib.file_test(`${XDG_RUNTIME_DIR}/hypr/${HIS}/.socket.sock`, GLib.FileTest.EXISTS) ?
-                                `${XDG_RUNTIME_DIR}/hypr/${HIS}/.socket.sock` : `/tmp/hypr/${HIS}/.socket.sock`;
-        const connection = socket(socketPath);
+        const connection = this._connection('socket');
 
         connection
             .get_output_stream()
